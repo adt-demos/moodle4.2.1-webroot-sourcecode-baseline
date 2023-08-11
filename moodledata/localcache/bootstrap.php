cd76cfc5183ddb017c7a2cef06dc54bc69e899b3
Templated User <templateduser@dummyemailX123.com>
2023-08-11 20:48:13 +0100

    Baseline Baby

---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = '2W4c2eXVeZ8VyWhpnxmgFpvt7RAcxiIQapplicationdomainwww.tld';
+$CFG->bootstraphash = '03caaef35eafcd5d575fdea516ab7d4b';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
